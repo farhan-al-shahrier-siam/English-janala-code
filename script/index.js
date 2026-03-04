f2cb76c2e148ada6c24e56846fbfce3151dc2fe5
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-03-04 12:06:41 +0600

    Lessons buttons are no with toggle effect

--- script/index.js
+++ script/index.js
@@ -4,33 +4,54 @@ const loadLessons = () => {
         .then((json) => displayLessons(json.data));
 };
 
+const removeActive = () => {
+    const lessonBtns = document.querySelectorAll(".lesson-btn");
+    lessonBtns.forEach((btn) => btn.classList.remove("active"));
+};
+
 const loadLevelWord = (id) => {
     // console.log(id);
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     // console.log(url);
     fetch(url)
         .then((res) => res.json())
-        .then((data) => displayLevelWord(data.data));
+        .then((data) => {
+            removeActive();
+            const clickBtn = document.getElementById(`lesson-btn-${id}`);
+            clickBtn.classList.add("active");
+            displayLevelWord(data.data);
+        });
 };
 
 const displayLevelWord = (words) => {
-    console.log(words);
+    // console.log(words);
     const wordContainer = document.getElementById("word-container");
     wordContainer.innerHTML = "";
+
+    if (words.length == 0) {
+        wordContainer.innerHTML = `
+        <div class="col-span-full text-center space-y-6 font-bangla p-10">
+                <img class="mx-auto" src="./assets/alert-error.png"
+                <p class="text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                <p class="font-bold text-4xl">নেক্সট Lesson এ যান</p>
+            </div>
+        `;
+        return;
+    }
     for (let word of words) {
         const wordDiv = document.createElement("div");
         wordDiv.innerHTML = `
             <div class="bg-white rounded-xl shadow-sm text-center py-16 px-5 space-y-4">
-                <h2 class="text-2xl font-bold">${word.word}</h2>
+                <h2 class="text-2xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
                 <p class="font-semibold">Meaning /Pronounciation</p>
-                <div class="text-xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+                <div class="text-xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যায়নি"}"</div>
                 <div class="flex justify-between items-center">
                     <button class="btn rounded-full"><i class="fa-solid fa-circle-info"></i></button>
                     <button class="btn rounded-full"><i class="fa-solid fa-volume-high"></i></button>
                 </div>
             </div>
         `;
-        wordContainer.appendChild(wordDiv)
+        wordContainer.appendChild(wordDiv);
     }
 };
 
@@ -43,7 +64,7 @@ const displayLessons = (lessons) => {
         // 3. create elements
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-primary btn-outline"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
+        <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-primary btn-outline lesson-btn"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
         `;
         levelContainer.appendChild(btnDiv);
     }
